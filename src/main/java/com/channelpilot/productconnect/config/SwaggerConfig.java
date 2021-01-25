7a3ead8f687602ffc9a099a294a2932d9fec98f8
nazila.golchin <nazila.golchin@4elements-gruppe.de>
2021-01-25 09:45:33 +0100

    commit changes

--- src/main/java/com/channelpilot/productconnect/config/SwaggerConfig.java
+++ src/main/java/com/channelpilot/productconnect/config/SwaggerConfig.java
@@ -2,6 +2,8 @@ package com.channelpilot.productconnect.config;
 
 import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 
+import java.util.Collections;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -20,40 +22,27 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 public class SwaggerConfig {
 
 	private static final String DESCRIPTION = "Documentation of the channelpilot REST API to connect a channelpilot to external aplications. \r\n"
-				+ "\r\n"
-				+ "Authorization & security\r\n"
-				+ "\r\n"
-				+ "authentication API endpoint URL is : /api/user\r\n"
-				+ "\r\n"
-				+ "every request has to be sent over HTTPS and must :\r\n"
-				+ "\r\n"
-				+ "Contain a valid API JWT Token get from /api/user endpoint . You'll have to provide your API token via Authorization-Header: Authorization: Bearer yourToken.Every call to API is secured by an API token\r\n"
-				+ "\r\n"
-				+ "Get API token\r\n"
-				+ "\r\n"
-				+ "To enabe access to the REST API, a user has to be get token by username and password from /api/user API Afterwards, a randomly generated API access key will be provided, which needs to be included in your API requests for authentication.";
+			+ "\r\n" + "Authorization & security\r\n" + "\r\n" + "authentication API endpoint URL is : /api/user\r\n"
+			+ "\r\n" + "every request has to be sent over HTTPS and must :\r\n" + "\r\n"
+			+ "Contain a valid API JWT Token get from /api/user endpoint . You'll have to provide your API token via Authorization-Header: Authorization: Bearer yourToken.Every call to API is secured by an API token\r\n"
+			+ "\r\n" + "Get API token\r\n" + "\r\n"
+			+ "To enabe access to the REST API, a user has to be get token by username and password from /api/user API Afterwards, a randomly generated API access key will be provided, which needs to be included in your API requests for authentication.";
 
-	public static final Contact DEFAULT_CONTACT = new Contact("Nazila Golchin", "https://github.com/nazilagolchin",
-			"nazila.golchin@gmail.com");
-
-	public static final ApiInfo V1_API_INFO = new ApiInfo("channelpilot API 1.0",
-			DESCRIPTION
-			+ "",
-			"1.0", "urn:tos", DEFAULT_CONTACT, "Apache 2.0", "http://www.apache.org/licenses/LICENSE-2.0");
-
-	public static final ApiInfo V2_API_INFO = new ApiInfo("channelpilot API 2.0",
-			DESCRIPTION, "2.0",
-			"urn:tos", DEFAULT_CONTACT, "Apache 2.0", "http://www.apache.org/licenses/LICENSE-2.0");
+	private ApiInfo apiInfo() {
+		return new ApiInfo("Channel Pilot API", DESCRIPTION, "API Product", "",
+				new Contact("Nazila Golchin", "https://github.com/nazilagolchin", "nazila.golchin@gmail.com"), "", "",
+				Collections.emptyList());
+	}
 
 	/**
 	 * @return Docket for version 1.0 builder for version 1.0
 	 */
 	@Bean
 	public Docket swaggerProductApi10() {
-		return new Docket(DocumentationType.SWAGGER_2).groupName("demorestapi-1.0").select()
+		return new Docket(DocumentationType.SWAGGER_2).groupName("Product-1.0").select()
 				.apis(RequestHandlerSelectors.basePackage("com.channelpilot.productconnect.controller"))
 				.paths(PathSelectors.ant("/api/v1/**")).build().produces(Sets.newHashSet(APPLICATION_JSON_VALUE))
-				.consumes(Sets.newHashSet(APPLICATION_JSON_VALUE)).apiInfo(V1_API_INFO);
+				.consumes(Sets.newHashSet(APPLICATION_JSON_VALUE)).apiInfo(apiInfo());
 	}
 
 	/**
@@ -61,9 +50,9 @@ public class SwaggerConfig {
 	 */
 	@Bean
 	public Docket swaggerProductApi20() {
-		return new Docket(DocumentationType.SWAGGER_2).groupName("demorestapi-2.0").select()
+		return new Docket(DocumentationType.SWAGGER_2).groupName("Product-2.0").select()
 				.apis(RequestHandlerSelectors.basePackage("com.channelpilot.productconnect.controller"))
 				.paths(PathSelectors.ant("/api/v2/**")).build().produces(Sets.newHashSet(APPLICATION_JSON_VALUE))
-				.consumes(Sets.newHashSet(APPLICATION_JSON_VALUE)).apiInfo(V2_API_INFO);
+				.consumes(Sets.newHashSet(APPLICATION_JSON_VALUE)).apiInfo(apiInfo());
 	}
 }
